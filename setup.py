c6f8d6ae03f47691ebaab056f169b5ac30b035d4
Madhurima Paul <79975103+MadhurimaPaul-87@users.noreply.github.com>
2021-07-10 21:16:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,13 @@ setup(
   author = 'Madhurima Paul',                   # Type in your name
   author_email = 'mguharoy4@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/MadhurimaPaul-87/packages',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/MadhurimaPaul-87/packages/archive/refs/tags/v_01.tar.gz'
   keywords = ['anova', 'SSB', 'SSW'],   # Keywords that define your package best
-     
+   install_requires=[            # I get to this in a second
+          'pandas',
+          'numpy',
+          'matplotlib',
+      ],   
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
